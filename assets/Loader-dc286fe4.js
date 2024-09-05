3c48909525d6932dcf86addab4cb2d5552847cdf
Rudra Hingu <rudrahingu07@gmail.com>
2024-09-05 18:47:54 +0530

    fourth commit

---
+++ assets/Loader-dc286fe4.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-775980a7.js";function i(){return t.jsx("div",{className:"fixed top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-black bg-opacity-50 z-50",children:t.jsx("h1",{className:"text-6xl font-bold text-white animate-pulse",children:"HEKAWAY"})})}export{i as default};
